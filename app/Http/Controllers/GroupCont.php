1b0240cbe671c49199ebfe590fb3a83c78b84701
FebriTahta <febrirtah@gmail.com>
2022-06-28 15:48:31 +0700

    show data anggota group

--- app/Http/Controllers/GroupCont.php
+++ app/Http/Controllers/GroupCont.php
@@ -84,7 +84,7 @@ class GroupCont extends Controller
 
             $group_id = $group_id;   
             $data = Karyawan::with('group', function($q) use ($group_id){
-                $q->where('id', $group_id);
+                return $q->where('id', $group_id);
             });
 
            if ($data !== null) {
