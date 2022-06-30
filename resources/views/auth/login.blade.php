94e4c9047e75fb0d2ca7b534c885c8707adb2279
FebriTahta <febrirtah@gmail.com>
2022-06-30 16:26:44 +0700

    tes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<link href="{{ asset('select2-develop/dist/css/select2.css') }}" rel="stylesheet" />
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
