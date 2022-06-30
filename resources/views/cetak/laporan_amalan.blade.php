039ca977b21e3a743cf1ac59245d54afb4dde6be
FebriTahta <febrirtah@gmail.com>
2022-06-30 15:19:13 +0700

    tes

--- resources/views/cetak/laporan_amalan.blade.php
+++ resources/views/cetak/laporan_amalan.blade.php
@@ -146,7 +146,7 @@
 
                     <td>
                         @if ($item->poin->max('besar_poin') * $jumHari > 0)
-                            {{round(($item->poin->max('besar_poin') * $jumHari)/$val)}} %
+                            {{($item->poin->max('besar_poin') * $jumHari)/$val}} 
                         @else
                         -
                         @endif
