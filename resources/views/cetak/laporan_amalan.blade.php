6ca4843b573253503ea20f1db6226a5cc621f31f
FebriTahta <febrirtah@gmail.com>
2022-06-30 14:08:49 +0700

    tes

--- resources/views/cetak/laporan_amalan.blade.php
+++ resources/views/cetak/laporan_amalan.blade.php
@@ -88,7 +88,7 @@
                                 $val = App\Models\Penilaian::where('karyawan_id', $data_karyawan->id)
                                                         ->where('jenis_id', $data_jenis->id)
                                                         ->where('kategori_id', $item->id)
-                                                        ->whereDate('created_at', $data_tahun.'-'.$bulan.'-22')
+                                                        ->whereDate('created_at', date('Y-m-d', strtotime($tanggal)))
                                                         ->first();
                             @endphp
                             <td>{{$tgl}}</td>
