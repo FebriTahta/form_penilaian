e66153e9a9a25a876d8e604f5b4a6450c4ce64e8
FebriTahta <febrirtah@gmail.com>
2022-06-30 14:12:49 +0700

    tes

--- resources/views/cetak/laporan_amalan.blade.php
+++ resources/views/cetak/laporan_amalan.blade.php
@@ -88,10 +88,14 @@
                                 $val = App\Models\Penilaian::where('karyawan_id', $data_karyawan->id)
                                                         ->where('jenis_id', $data_jenis->id)
                                                         ->where('kategori_id', $item->id)
-                                                        ->whereDate('created_at', date('Y-m-d'))
+                                                        ->whereDate('created_at', date('Y-m-d', strtotime($tanggal)))
                                                         ->first();
                             @endphp
-                            <td>{{$val->poin}}</td>
+                            <td>
+                                @if ($val !== null)
+                                {{$val->poin}}
+                                @endif
+                            </td>
                     @endforeach
                     
                 </tr>
