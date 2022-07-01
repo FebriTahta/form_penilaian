3e4297ed5a26f02ea235449c548027aa83b573c9
FebriTahta <febrirtah@gmail.com>
2022-07-01 10:36:12 +0700

    laporan group

--- app/Exports/LaporanGroupExport.php
+++ app/Exports/LaporanGroupExport.php
@@ -8,16 +8,59 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 
 class LaporanGroupExport implements ShouldAutoSize,FromView
 {
-    // public function __construct($month,$jenis,$bulan,$tahun)
-    // {
-    //     $this->month=$month;
-    //     $this->jenis=$jenis;
-    //     $this->bulan=$bulan;
-    //     $this->tahun=$tahun;
-    // }
+    public function __construct($jenis,$bulan,$tahun,$month)
+    {
+        $this->month=$month;
+        $this->jenis=$jenis;
+        $this->bulan=$bulan;
+        $this->tahun=$tahun;
+    }
 
     public function view(): View
     {
-        return view('cetak.laporan_amalan_group');
+        $data_month    = $this->month;
+        $data_jenis    = $this->jenis;
+        $bulan         = $this->bulan;
+        $data_tahun    = $this->tahun;
+        $data_bulan    = 0;
+
+        if ($bulan == '01') {
+            # code...
+            $data_bulan = 'Januari';
+        }elseif($bulan == '02'){
+            # code...
+            $data_bulan = 'Februari';
+        }elseif($bulan == '03'){
+            # code...
+            $data_bulan = 'Maret';
+        }elseif($bulan == '04'){
+            # code...
+            $data_bulan = 'April';
+        }elseif($bulan == '05'){
+            # code...
+            $data_bulan = 'Mei';
+        }elseif($bulan == '06'){
+            # code...
+            $data_bulan = 'Juni';
+        }elseif($bulan == '07'){
+            # code...
+            $data_bulan = 'Juli';
+        }elseif($bulan == '08'){
+            # code...
+            $data_bulan = 'Agustus';
+        }elseif($bulan == '09'){
+            # code...
+            $data_bulan = 'September';
+        }elseif($bulan == '10'){
+            # code...
+            $data_bulan = 'Oktober';
+        }elseif($bulan == '11'){
+            # code...
+            $data_bulan = 'November';
+        }elseif($bulan == '12'){
+            # code...
+            $data_bulan = 'Desember';
+        }
+        return view('cetak.laporan_amalan_group',compact('data_month','data_jenis','data_bulan','data_tahun','bulan'));
     }
 }
--- resources/views/cetak/laporan_amalan_group.blade.php
+++ resources/views/cetak/laporan_amalan_group.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> _ </title>
+    <title>{{$data_jenis->nama_jenis}}</title>
     <style>
         .notel {
         mso-number-format: "\@";
@@ -15,7 +15,7 @@
     <table>
         <thead style="font-weight: bold; text-transform: uppercase">
             <tr>
-                <th rowspan="3" colspan="13"> _ <br> <p> GROUP - Periode _</p></th>
+                <th rowspan="3" colspan="13"> {{$data_jenis->nama_jenis}} <br> <p> GROUP - Periode {{$data_bulan}} {{$data_tahun}}</p></th>
             </tr>
         </thead>
     </table>
@@ -28,6 +28,14 @@
     {{-- spasi --}}
     
 
+    @foreach ($jenis->group as $item)
+    <table>
+        <thead style="font-weight: bold; text-transform: uppercase">
+            <tr>
+                <th rowspan="2" colspan="6"> {{$item->nama_group}}</th>
+            </tr>
+        </thead>
+    </table>
     <table style="border: solid">
         <thead>
             <tr>
@@ -45,5 +53,7 @@
             
         </tbody>
     </table>
+    @endforeach
+    
 </body>
 </html>
