e1e62bb7cd63835fc4f83469bf71e4f45495c898
FebriTahta <febrirtah@gmail.com>
2022-07-01 11:11:57 +0700

    laporan group

--- resources/views/cetak/laporan_amalan_group.blade.php
+++ resources/views/cetak/laporan_amalan_group.blade.php
@@ -69,9 +69,9 @@
                                                     ->whereMonth('tanggal', $bulan)
                                                     ->whereYear('tanggal', $data_tahun)
                                                     ->sum('nilai');
-                            $nilai[] = $val;
-                            $real = array_sum($nilai);
                         }
+                        $nilai[] = $val;
+                        $real = array_sum($nilai);
                     @endphp
                     <td>{{implode(',',$nilai)}}</td>
                 </tr>
